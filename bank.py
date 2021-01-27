b0465422b1d1f381bbaf5fd385e90196e13cf871
maxiaocong <3041208179@qq.com>
2021-01-27 18:27:09 +0800

    第二次提交bank.py

--- bank.py
+++ bank.py
@@ -27,7 +27,6 @@ welcome = '''
 *********************************
 '''
 
-
 # 专门来获取8位随机账号
 def getRandom():
     li = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
@@ -37,7 +36,6 @@ def getRandom():
         ch = li[random.randint(0, 9)]
         string = string + str(ch)
     return string
-
 
 # 银行的核心开户方法
 def bank_addUser(account, username, password, money, country, province, street, door):
@@ -62,27 +60,18 @@ def bank_addUser(account, username, password, money, country, province, street, 
         }
         return 1
 
-
 def bank_savemoney(username, money):
-    i = users.values()
-    for value in i:
-        if username in value.values():
-            value["money"] = value["money"] + money
-            return 1
-        else:
-            return 2
-
+    i = users.values()    # 字典里面的值给了i
+    if username in users:  # 判断用户里面有没有用户名
+        users[username]["money"]=money+users[username]["money"]
+        return 1
+    else:
+        return 2
 
 def query():
     username = input("请输入用户名:")
     password = input("请输入密码:")
-    money = users[username]["money"]
-    country = users[username]["country"]
-    province = users[username]["province"]
-    street = users[username]["street"]
-    door = users[username]["door"]
-    savequery = bank_query(username, password, money, country, province, street, door)
-
+    savequery = bank_query(username, password)
     if savequery == 1:
         print("您的用户名为:", username)
         print("您的密码为:", users[username]["password"])
@@ -98,45 +87,31 @@ def query():
         print("该用户不存在")
 
 
-def bank_query(username, password, money, country, province, street, door):
-    a = users.values()  # 字典里所有值给了a
-    if username in users:  # 判断字典里面用户中有没有用户名
-        for i in a:  # 遍历所有值
-            if password in i.values():
-                users[username] = {
-                    "password": password,
-                    "money": money,
-                    "country": country,
-                    "province": province,
-                    "street": street,
-                    "door": door,
-                    "bank_name": bank_name
-                }
+def bank_query(username, password):
+    a = users.values()  #把值赋给a
+    if username in users:  #判断用户里面有没有用户名
+        for i in a:  #遍历字典
+            if password in users:   #
                 return 1
             else:
                 return 2
     else:
         return 3
-
 # 银行的核心取款方法
 def bank_withdraw_money(username,password,money):
-    for value in users.values():
-        # 判断是否已经存在
-        if username not in users.keys(): # 这种方式只判断是否在字典的键里存在
-            return 1
-        # 密码不对
-        elif password!=value["password"]:
-            return 2
-        elif money>value["money"]:
-            return 3
-        # 可以正常开户：将个人数据存到用户库里
-        else:
-            value["money"]=value["money"]-money
-            users[username] = {
-                "password":password,
-                "money":value["money"],
-            }
-            return 0
+    a = users.values()     # 把值赋给a
+    if username in users:  # 判断用户里面有没有用户名
+        for i in a:   # 遍历字典
+            if password in users:
+                if (money < users[username]["money"]) or (money == users[username]["money"]):
+                    users[username]["money"]=users[username]["money"]-money
+                    return 1
+                else:
+                    return 3
+            else:
+                return 2
+    else:
+        return 0
 
 def withdraw_money():
     username = input("请输入用户名：")
@@ -144,33 +119,30 @@ def withdraw_money():
     money = int(input("请输入您的取款金额："))
     # 将数据传给银行
     status1=bank_withdraw_money(username,password,money)
-    if status1==1:
+    if status1==0:
         print("您输入的账号不存在！")
     elif status1==2:
         print("您输入的密码错误！")
     elif status1==3:
         print("您的余额不足！")
-    elif status1==0:
-        print("取款成功！您当前的余额为：",users[username]["money"])
+    elif status1==1:
+        print("取款成功！您当前的余额为：",username,users[username]["money"])
 
 def savemoney():
-    username = input("请输入用户名:")
-    money = input("请输入金额:")
-
+    username=input("请输入用户名:")
     while True:
+        money = input("请输入金额:")
         if money.isdigit():
             money = int(money)
             break
         else:
             print("余额输入错误，请重新输入！")
-
+    print(users[username]["money"])
     returnvalue = bank_savemoney(username, money)
     if returnvalue == 1:
-        print("存钱成功！！！您存的金额为:", money, "￥", "余额为:", users[username]["money"], "￥")
-        print(users)
+        print("存钱成功！！！","您的账号为:",username,"您的余额为:",users[username]["money"])
     else:
         print("查无此用户！！！")
-
 
 # 普通的开户方法
 def addUser():
@@ -181,7 +153,7 @@ def addUser():
     money = int(input("请输入您的初始金额："))  # 金额是整数形式
     print("接下来输入地址信息：")
     country = input("请输入您所在国家：")
-    province = input("亲输入您所在省份：")
+    province = input("请输入您所在省份：")
     street = input("请输入您所在的街道：")
     door = input("请输入您地址的门牌号：")
     account = getRandom()  # 获取随机账号
@@ -206,7 +178,6 @@ def addUser():
         print("开户行名为:", users[username]["bank_name"])
         print(users)
 
-
 def bank_transfer(out_username, up_username, out_pass, out_money):
     if out_username in users and up_username in users:
         if out_pass == users[out_username]["password"]:
@@ -219,7 +190,6 @@ def bank_transfer(out_username, up_username, out_pass, out_money):
     else:
         return 1
 
-
 def transferMoney():
     out_username = input("请输入转出账号:")
     up_username = input("请输入转入账号:")
@@ -230,14 +200,13 @@ def transferMoney():
     if status5 == 1:
         print("转出账户或转入账户不存在！")
     elif status5 == 2:
-        print("密码不对！")
+        print("密码不正确！")
     elif status5 == 3:
-        print("钱不够！")
+        print("余额不足！")
     elif status5 == 0:
         users[out_username]["money"] = users[out_username]["money"] - out_money
         users[up_username]["money"] = users[up_username]["money"] + out_money
-        print("转账成功！")
-
+        print("转账成功！","转出账号为:",out_username,"转出账号余额为:",users[out_username]["money"],"转入账号为:",up_username,"转入账号余额为:",users[up_username]["money"])
 
 while True:
     print(welcome)  # 打印欢迎信息
